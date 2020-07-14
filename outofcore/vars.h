7a0a11c166c2d7a7c99926e970ab695fd1336995
Mert Hidayetoglu <merth@login3.summit.olcf.ornl.gov>
2020-07-14 01:04:40 -0400

    out-of-core implementation

---
+++ outofcore/vars.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <omp.h>
+#include "mma.h"
+
+using namespace std;
+
+void readweights();
+void preproc();
+void readinput();
+
+void setup_gpu();
+void final_gpu();
+void infer_gpu(int);
+
+#define OUTOFCORE //COMMENT THIS OUT IF YOU HAVE ENOUGH MEMORY
+#define WARPSIZE 32
+#define MINIBATCH 12
+#define MAPPREC unsigned short
+#define INDPREC unsigned short
+#define VALPREC float
+#define FEATPREC float
+
